253417a3803bdd32950422aae3efb298fab17f9a
unknown <lfdias2203@gmail.com>
2025-01-17 00:06:03 -0400

    atualizacao resume

--- main.js
+++ main.js
@@ -264,22 +264,6 @@ document.querySelector('#app').innerHTML = `
           </a>
     </div>
     <div class="flex items-center ml-4 my-4">
-    </div>
-      <button onclick="window.open('/assets/LuizFelipe_DiasdeAraujo_CurrículoAtual.pdf', '_blank')" class="cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black bg-opacity-80 text-[#f1f1f1] rounded-lg hover:bg-opacity-70 transition font-semibold shadow-md">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24px" width="24px">
-          <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-          <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
-          <g id="SVGRepo_iconCarrier">
-            <g id="Interface / Download">
-              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="#f1f1f1" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" id="Vector"></path>
-            </g>
-          </g>
-        </svg>
-        Resume
-        <div class="absolute opacity-0 -bottom-full rounded-md py-2 px-2 bg-black bg-opacity-70 left-1/2 -translate-x-1/2 group-hover:opacity-100 transition-opacity shadow-lg">
-        </div>
-      </button>
-      </div>
   </div>
 </div>
 </div>
